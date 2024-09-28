b0c535cc296d5970f7e9fbd2fd73bb47ce3d02b1
슬 <seul@I-got-no-energy.local>
2024-09-29 02:03:48 +0900

    :sparkles:feat: Cleanup function 개념 이해하기

--- src/App.js
+++ src/App.js
@@ -1,31 +1,21 @@
-import Button from './Button';
-import styled from './App.module.css';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log('생성됨'); // 마운트 될 때 실행
+    return () => console.log('삭제됨'); // return 이후 : 마운트 해제 될 때 실행
+  },[])
+  return <h1>Hello</h1>;
+}
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState('');
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-
-  console.log('I run all the time'); // 모든 업데이트가 있을 때마다 리렌더링
-
-  useEffect(() => {
-    console.log('call the API')
-  }, []); // 처음 렌더링 이후 조건부 (조건이 없으므로 1회성)
-
-  useEffect(() => {
-    if (keyword !== '') { // 키워드가 있을 때만 렌더링 시작
-      console.log('SEARCH FOR', keyword);
-    }
-  }, [keyword]); // 처음 렌더링 이후 keyword가 업데이트 될 때마다 리렌더링
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
 
   return (
     <div>
-      <input value={keyword}
-       onChange={onChange} type='text' placeholder='Search hear' />
-      <h1>{counter}</h1>
-      <Button onClick={onClick}>click me</Button>
+      <button onClick={onClick}>{showing ?  'Hide' : 'Show'}</button>
+      {showing ? <Hello/> : null}
     </div>
   );
 }
