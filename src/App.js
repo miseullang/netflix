9475edf9f18129394694743931ce5fe27e4985b5
슬 <seul@I-got-no-energy.local>
2024-10-04 00:16:56 +0900

    :sparkles:feat: 영화 정보 받아오기

--- src/App.js
+++ src/App.js
@@ -40,7 +40,21 @@ console.log(movies);
 
   return (
     <div>
-      {loading ? <h1>Loading...</h1> : null}
+      {loading ? (<h1>Loading...</h1>) : (
+        <div>{movies.map((movie) => (
+          <div key={movie.id}>
+            <img src={movie.medium_cover_image} />
+            <h2>{movie.title}</h2>
+            <p>{movie.summary}</p>
+            <ul>
+              {movie.genres?.map((genre) => (
+                <li key={genre}>{genre}</li>
+              ))}
+            </ul>
+          </div>
+          ))}
+        </div>
+      )}
     </div>
   )
 }
