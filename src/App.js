8e5bb4196467358ab63a49ec0f77bba409fc3226
슬 <seul@I-got-no-energy.local>
2024-10-03 23:53:31 +0900

    :memo:docs: fetch-then / async-await 비교

--- src/App.js
+++ src/App.js
@@ -2,27 +2,47 @@ import React, { useEffect, useState } from 'react'
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // async-await 사용 (짧은 버전)
+    const json = await (
+      await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
 
+    // async-await 사용 (조금 더 긴 버전)
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    // );
+    // const json = await response.json();
+    // setMovies(json.data.movies);
+    // setLoading(false);
+  };
   useEffect(() => {
-    fetch('https://api.coinpaprika.com/v1/tickers')
-      .then(response => response.json())
-        .then((json) => {
-          setCoins(json);
-          setLoading(false);
-        });
-  }, [])
+    getMovies();
+  },[]);
+
+console.log(movies);
+
+  // fetch만 사용
+  // useEffect(() => {
+  //   fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   ).then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // },[]);
+
   return (
     <div>
-      <h1>The Coins! ({coins.length})</h1>
-      {loading ? <strong>Loading...</strong>:
-        <select>
-          {coins.map((coin,index) =>  
-            (<option key={index}>{coin.name} ({coin.symbol}):${coin.quotes.USD.price} USD</option>))}
-        </select>}
-      
+      {loading ? <h1>Loading...</h1> : null}
     </div>
-  );
+  )
 }
 
 export default App
