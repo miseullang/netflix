adbd1a63f595b11ab1a14bd8059e2478e47dc899
슬 <seul@I-got-no-energy.local>
2024-10-04 09:54:49 +0900

    :sparkles:feat: Router 추가

--- src/App.js
+++ src/App.js
@@ -1,62 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom'
+import Home from './routes/Home';
+import Detail from './routes/Detail';
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    // async-await 사용 (짧은 버전)
-    const json = await (
-      await fetch(
-      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-      )
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-
-    // async-await 사용 (조금 더 긴 버전)
-    // const response = await fetch(
-    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-    // );
-    // const json = await response.json();
-    // setMovies(json.data.movies);
-    // setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  },[]);
-
-console.log(movies);
-
-  // fetch만 사용
-  // useEffect(() => {
-  //   fetch(
-  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
-  //   ).then((response) => response.json())
-  //     .then((json) => {
-  //       setMovies(json.data.movies);
-  //       setLoading(false);
-  //     });
-  // },[]);
-
   return (
-    <div>
-      {loading ? (<h1>Loading...</h1>) : (
-        <div>{movies.map((movie) => (
-          <div key={movie.id}>
-            <img src={movie.medium_cover_image} />
-            <h2>{movie.title}</h2>
-            <p>{movie.summary}</p>
-            <ul>
-              {movie.genres?.map((genre) => (
-                <li key={genre}>{genre}</li>
-              ))}
-            </ul>
-          </div>
-          ))}
-        </div>
-      )}
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movie' element={<Detail/>} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App
---
+++ src/routes/Home.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react'
+import Movie from '../Movie';
+
+function Home() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    // async-await 사용 (짧은 버전)
+    const json = await (
+      await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+
+    // async-await 사용 (조금 더 긴 버전)
+    // const response = await fetch(
+    //   `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+    // );
+    // const json = await response.json();
+    // setMovies(json.data.movies);
+    // setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  },[]);
+
+console.log(movies);
+
+  // fetch만 사용
+  // useEffect(() => {
+  //   fetch(
+  //     `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.5&sort_by=year`
+  //   ).then((response) => response.json())
+  //     .then((json) => {
+  //       setMovies(json.data.movies);
+  //       setLoading(false);
+  //     });
+  // },[]);
+
+  return (
+    <div>
+      {loading ? (<h1>Loading...</h1>) : (
+        <div>{movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            coverImg={movie.medium_cover_image}
+            title={movie.title}
+            summary={movie.summary}
+            genres={movie.genres}/>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+
+}
+
+export default Home
